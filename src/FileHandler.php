113603f1161b230cce7a333a61fd73dcfde40c0e
Алборов Роман Васильевич <r.alborov@efko.ru>
2023-12-14 00:58:51 +0300

    [TRNTYFRMWR-20231214] добавлены алиасы

---
+++ src/FileHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace trinity;
+
+use trinity\contracts\FileHandlerInterface;
+
+class FileHandler implements FileHandlerInterface
+{
+    private array $aliases;
+
+    public function __construct(array $config)
+    {
+        $this->aliases = $config;
+    }
+
+    public function getAlias(string $link): ?string
+    {
+        return $this->aliases[$link] ?? null;
+    }
+
+    public function aliasExists(string $link): bool
+    {
+        return array_key_exists($link, $this->aliases);
+    }
+
+    public function setAlias(string $link, string $path): void
+    {
+        $this->aliases[$link] = $path;
+    }
+
+    public function resolvePath(string $path): string
+    {
+        foreach ($this->aliases as $alias => $aliasPath) {
+            if (strpos($path, $alias) === 0) {
+                return str_replace($alias, $aliasPath, $path);
+            }
+        }
+
+        return $path;
+    }
+}
