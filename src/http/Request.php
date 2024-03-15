78d7ffaf7c20a9fd55ddb80921c245343a71ec99
Артур Ушаков <a.s.ushakov@efko.ru>
2024-03-16 00:15:06 +0300

    правки

--- src/http/Request.php
+++ src/http/Request.php
@@ -31,7 +31,7 @@ class Request extends BaseRequest implements RequestInterface
     {
         parent::__construct($server['REQUEST_METHOD'], $server['REQUEST_URI'], getallheaders());
         $this->queryParams = $get;
-        $this->contentType = $_SERVER['CONTENT_TYPE'] !== '' ? $_SERVER['CONTENT_TYPE'] : $_SERVER['HTTP_ACCEPT'];
+        $this->contentType = 'application/json';
         $this->input = $post;
         $this->cookie = $cookie;
     }
